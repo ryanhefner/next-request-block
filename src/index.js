1c99758b11dc462f222cc597871b888a62604743
Ryan Hefner <hi@ryanhefner.com>
2019-06-24 18:38:59 -0400

    Fix other outdated import

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default as initRequestBlockCache } from './initRequestBlock';
+export { default as initRequestBlockCache } from './initRequestBlockCache';
 export { default as withRequestBlock } from './withRequestBlock';
